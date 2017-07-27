a74f38e05bec4886b8093cc465db01b1e193ffee
NamanArora <naman.arora11@gmail.com>
2017-07-27 21:58:11 +0530

    Revert "Fix up defines and remove irrelevant function declaration"
    This reverts commit 3bf709f78b220332e0a593d1753dc259c19cfd61.

--- hci/include/hci_internals.h
+++ hci/include/hci_internals.h
@@ -17,13 +17,8 @@
  ******************************************************************************/
 
 #pragma once
-#ifdef HAS_BDROID_BUILDCFG
-#include "bdroid_buildcfg.h"
-#endif
 
-#ifndef REMOVE_EAGER_THREADS
 #define REMOVE_EAGER_THREADS TRUE
-#endif
 
 #if (defined(REMOVE_EAGER_THREADS) && (REMOVE_EAGER_THREADS == TRUE))
 #include "osi/include/allocator.h"
